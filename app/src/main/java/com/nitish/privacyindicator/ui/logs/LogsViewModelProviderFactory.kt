99d65ec3530dfe67f9aee61d33a1b83ad1cfe14d
puneetnith28 <puneetnith28@gmail.com>
2026-03-10 00:07:05 +0530

    Created basic structure

---
+++ app/src/main/java/com/nitish/privacyindicator/ui/logs/LogsViewModelProviderFactory.kt
@@ -0,0 +1,16 @@
+package com.nitish.privacyindicator.ui.logs
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.nitish.privacyindicator.repository.AccessLogsRepo
+
+class LogsViewModelProviderFactory(
+        private val application: Application,
+        private val accessLogsRepo: AccessLogsRepo
+): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return AccessLogsViewModel(application, accessLogsRepo) as T
+    }
+}
